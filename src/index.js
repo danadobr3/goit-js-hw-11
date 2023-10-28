ebdf43cdb0f2af9d68a247b22b37725896879d59
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-10-28 21:40:27 +0200

    redo all my hw

---
+++ src/index.js
@@ -0,0 +1,115 @@
+import NewsApiSearch from './js/api-search';
+import { lightbox } from './js/lightbox';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+let isShown = 0;
+const newsApiSearch = new NewsApiSearch();
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+const options = {
+  rootMargin: '50px',
+  root: null,
+  threshold: 0.3,
+};
+
+const watcher = new Watcher(onLoadMore, options);
+
+function onSearch(element) {
+    element.preventDefault();
+    
+    refs.gallery.innerHTML = '';
+    newsApiSearch.query =
+        element.currentTarget.elements.searchQuery.value.trim();
+    newsApiSearch.resetPage();
+
+    if (newsApiSearch.query === '') {
+        Notify.failure(`Please, fill the main field`);
+        return;
+    }
+
+    isShown = 0;
+  fetchGallery();
+  onRenderGallery(hits);
+}
+
+function onLoadMore() {
+  newsApiSearch.incrementPage();
+  fetchGallery();
+}
+
+async function fetchGallery() {
+    refs.loadMoreBtn.classList.add('is-hidden');
+    
+    const result = await newsApiSearch.fetchGallery();
+    const { hits, total } = result;
+    isShown += hits.length;
+    
+     if (!hits.length) {
+    Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`
+    );
+    refs.loadMoreBtn.classList.add('is-hidden');
+    return;
+     }
+    
+    onRenderGallery(hits);
+  isShown += hits.length;
+
+  if (isShown < total) {
+    Notify.success(`Hooray! We found ${total} images !!!`);
+    refs.loadMoreBtn.classList.remove('is-hidden');
+  }
+
+     if (isShown >= total) {
+    Notify.info(`We're sorry, but you've reached the end of search results.`);
+  }
+}
+
+function onRenderGallery(elements) {
+  const markup = elements
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+    <a href="${largeImageURL}">
+      <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+    </a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        ${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+        ${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        ${downloads}
+      </p>
+    </div>
+    </div>`;
+      }
+    )
+    .join('');
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
