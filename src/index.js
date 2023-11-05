523ca054ed5f4bbf891fd551c97051d1c1fe1b8a
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-11-05 17:08:54 +0100

    fix3

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ const refs = {
 
 let isShown = 0;
 const newsApiSearch = new NewsApiSearch();
-let currentPage = 1;
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
@@ -26,65 +25,68 @@ const options = {
 
 const observer = new IntersectionObserver(onLoadMore, options);
 
-function onSearch(event) {
-  event.preventDefault();
+ function onSearch(event) {
+    event.preventDefault();
+    
+    refs.gallery.innerHTML = '';
+    newsApiSearch.query = event.currentTarget.elements.searchQuery.value.trim();
+    newsApiSearch.resetPage();
 
-  refs.gallery.innerHTML = '';
-  newsApiSearch.query = event.currentTarget.elements.searchQuery.value.trim();
-  newsApiSearch.resetPage();
-  currentPage = 1;
+    if (newsApiSearch.query === '') {
+        Notify.failure(`Please, fill the main field`);
+        return;
+    }
 
-  if (newsApiSearch.query === '') {
-    Notify.failure(`Please, fill the main field`);
-    return;
-  }
-
-  isShown = 0;
+    isShown = 0;
   fetchGallery();
   onRenderGallery(hits);
 }
 
 async function fetchGallery() {
-  refs.loadMoreBtn.classList.add('is-hidden');
-
-  const result = await newsApiSearch.fetchGallery(currentPage);
-  const { totalHits, hits: newHits } = result;
-
-  if (newHits.length === 0) {
-    Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
     refs.loadMoreBtn.classList.add('is-hidden');
+    
+    const result = await newsApiSearch.fetchGallery();
+    const { totalHits } = result;
+    hits = result.hits;
+
+    isShown += hits.length;
+    
+    if (!hits.length) {
+        Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
+        refs.loadMoreBtn.classList.add('is-hidden');
+        return result;
+    }
+    
+    onRenderGallery(hits);
+    isShown += hits.length;
+
+    if (isShown < totalHits) {
+        Notify.success(`Hooray! We found ${totalHits} images !!!`);
+        refs.loadMoreBtn.classList.remove('is-hidden');
+    }
+
+    if (isShown >= totalHits) {
+        Notify.info("We're sorry, but you've reached the end of search results.");
+    }
     return result;
-  }
-
-  hits = [...hits, ...newHits];
-  isShown += newHits.length;
-
-  if (isShown < totalHits) {
-    Notify.success(`Hooray! We found ${totalHits} images !!!`);
-    refs.loadMoreBtn.classList.remove('is-hidden');
-  }
-
-  if (isShown >= totalHits) {
-    Notify.info("We're sorry, but you've reached the end of search results.");
-  }
-  currentPage++;
-  return result;
 }
 
-function onLoadMore(entries) {
-  const target = entries[0];
-  if (target.isIntersecting) {
+function onLoadMore() {
+  if (!refs.loadMoreBtn.classList.contains('is-hidden')) {
+    newsApiSearch.incrementPage();
     fetchGallery().then(result => {
       const { height: cardHeight } = document.querySelector(".gallery").lastElementChild.getBoundingClientRect();
 
       window.scrollBy({
-        top: cardHeight * 2,
+        top: cardHeight,
         behavior: "smooth",
       });
       initLightbox();
     });
+    }
   }
-}
+
+
 
 function onRenderGallery(elements) {
   const markup = elements
@@ -131,5 +133,3 @@ function onRenderGallery(elements) {
 function initLightbox() {
   lightbox.refresh();
 }
-
-observer.observe(refs.loadMoreBtn);
